b34bb8c61ee3e317d791fe530077964e932282ed
mihirsar <116589747+mihirsar@users.noreply.github.com>
2024-02-13 13:08:10 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask,request, url_for, redirect, render_template, jsonify
+from pycaret.regression import *
+import pandas as pd
+import pickle
+import numpy as np
+
+app = Flask(__name__)
+
+model = load_model('gbrmodel')
+cols = ['age', 'sex', 'bmi', 'children', 'smoker', 'region']
+
+@app.route('/')
+def home():
+    return render_template("home.html")
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    int_features = [x for x in request.form.values()]
+    final = np.array(int_features)
+    data_unseen = pd.DataFrame([final], columns=cols)
+    prediction = predict_model(model, data=data_unseen, round=0)
+    predicted_value = prediction['prediction_label'].iloc[0]  # Access the predicted value from the DataFrame
+    return render_template('home.html', pred='Expected Bill will be {}'.format(predicted_value))
+
+
+
+@app.route('/predict_api',methods=['POST'])
+def predict_api():
+    data = request.get_json(force=True)
+    data_unseen = pd.DataFrame([data])
+    prediction = predict_model(model, data=data_unseen)
+    output = prediction.Label[0]
+    return jsonify(output)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
